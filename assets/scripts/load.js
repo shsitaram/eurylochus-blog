62a7578ddb51730faef7a3342ffbdbfed2099e20
ShivSitaram <104377732+shsitaram@users.noreply.github.com>
2024-02-29 18:55:34 -0800

    reorganized files and organized js code

--- assets/scripts/load.js
+++ assets/scripts/load.js
@@ -1,3 +1,14 @@
+function loadfile(url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = () => {
+        if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.send(null);
+}
+
 // load blog entries
 for (let i = 1; i < 11; i++) {
     let main_node = document.createElement("div");
@@ -7,31 +18,16 @@ for (let i = 1; i < 11; i++) {
     let h2 = document.createElement("h2");
     h2.setAttribute("id", "blog-" + i + "-h2");
     h2.setAttribute("class", "blog-header");
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", 'assets/text/dates.txt', true);
-    xhr.onload = () => {
-        if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-            let s = xhr.responseText;
-            s = s.split("\n");
-            h2.innerHTML = s[i - 1];
-        }
-    }
-    xhr.send(null);
+    loadfile("assets/text/dates.txt", (s) => {
+        h2.innerHTML = s.split("\n")[i - 1];
+    });
 
     let p = document.createElement("p");
     p.setAttribute("id", "blog-" + i + "-p");
     p.setAttribute("class", "blog-paragraph");
-    let xhr2 = new XMLHttpRequest();
-    xhr2.open("GET", 'assets/text/blogs/' + i + '.txt', true);
-    xhr2.onload = () => {
-        if (xhr2.readyState === xhr2.DONE && xhr2.status === 200) {
-            let s = xhr2.responseText;
-            s = s.replaceAll("\n", "<br>");
-            p.innerHTML = s;
-        }
-    }
-    xhr2.send(null);
-
+    loadfile(`assets/text/blogs/${i}.txt`, (s) => {
+        p.innerHTML = s.replaceAll("\n", "<br>");
+    });
 
     main_node.appendChild(h2);
     main_node.appendChild(p);
@@ -45,16 +41,10 @@ for (let i = 1; i < 11; i++) {
         let par = document.createElement("p");
         par.setAttribute("id", "blog-" + i + "-com");
         par.setAttribute("class", "comment");
-        let xhr3 = new XMLHttpRequest();
-        xhr3.open("GET", 'assets/text/comments/' + i + '.txt', true);
-        xhr3.onload = () => {
-            if (xhr3.readyState == xhr3.DONE && xhr3.status == 200) {
-                let s = xhr3.responseText;
-                s = s.split('\n');
-                par.innerHTML = '<em><b>' + s[0] + ' (' + s[1] + '):</b> ' + s[2] + '</em>';
-            }
-        }
-        xhr3.send(null);
+        loadfile(`assets/text/comments/${i}.txt`, (s) => {
+            s = s.split("\n");
+            par.innerHTML = `<em><b>${s[0]} (${s[1]}):</b> ${s[2]}</em>`;
+        });
         main_node.appendChild(h3);
         main_node.appendChild(par);
     }
@@ -69,14 +59,9 @@ title.innerHTML = "Who am I?";
 
 let bio_content = document.createElement("p");
 bio_content.setAttribute("id", "bio-content");
-xhr = new XMLHttpRequest();
-xhr.open("GET", 'assets/text/bio.txt', true);
-xhr.onload = () => {
-    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-        bio_content.innerHTML = xhr.responseText;
-    }
-}
-xhr.send(null);
+loadfile("assets/text/bio.txt", (s) => {
+    bio_content.innerHTML = s;
+})
 
 document.getElementById("bio").appendChild(title);
 document.getElementById("bio").appendChild(bio_content);
